1a3aeaf7abe113eea994ebd24600f2d50a9ce1d5
CristianKreuzEngel <108903664+CristianKreuzEngel@users.noreply.github.com>
2022-10-02 11:14:44 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,23 @@
 let input =require('fs').readFileSync('./dev/stdin/flie.txt', 'utf8')
 let lines = input.split('\n')
-
-const lenha = (line) => line.split(" ").map(a => +a)
-const [a,b,c] = lenha(lines.shift())
-lista=[a,b,c]
-soma=Math.max(a,b,c)
-
-console.log(`${soma} eh o maior`)
+const pValor = (line) => line.split(" ").map(a => +a)
+let contA = 0
+let contB = 0
+while (true){
+    let caso =+lines.shift()
+    if (caso == 0){
+        break;
+    }
+    for (let i = 0; i < caso; i++){
+        let [a,b] = pValor(lines.shift())
+        if (a > b){
+            contA += 1
+        }
+        else if(a < b){
+            contB += 1
+        }
+    }
+    console.log(contA,contB)
+    contA = 0
+    contB = 0
+}
